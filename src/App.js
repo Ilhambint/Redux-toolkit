26687a63caa786e9f1e5ccd0fd49240b3e009316
Ilhambint <ilhambintangp7@gmail.com>
2022-04-25 13:54:48 +0700

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import counterSlice from './store/counter'
+import Burger from './Components/Burger/Burger'
+import Product from './Components/Product/Product'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //function dengan parameter store untuk mengambil state count di counter
+    const count = useSelector(store => store.counter.count)
+    const dispatch = useDispatch()
+
+    return (
+        <div className="App">
+            <h1>{count}</h1>
+            <button onClick={() => dispatch(counterSlice.actions.increment())}>INCREMENT</button>
+            <button onClick={() => dispatch(counterSlice.actions.decrement())}>DECREMENT</button>
+            <button onClick={() => dispatch(counterSlice.actions.increaseByNumber(5))}>Increment By 5</button>
+            <button onClick={() => dispatch(counterSlice.actions.increaseByNumber(10))}>Increase By 10</button>
+            <hr/>
+            <Burger/>
+            <hr/>
+            <Product/>
+        </div>
+    );
 }
 
 export default App;
